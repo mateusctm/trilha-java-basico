57d78a4c906ba51cd80d9cb6c036a35b809dace9
mateusctm <mateusctm@gmail.com>
2024-05-10 11:32:01 -0300

    Adicionandoi classe ContaTerminal, e processo de criação de conta em banco

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+
+        ContaTerminal contaTerminal = new ContaTerminal();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        contaTerminal.setAgencia(scanner.next());
+
+        System.out.println("Por favor, digite o número da Conta !");
+        contaTerminal.setNumero(scanner.nextInt());
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome de Usuário!");
+        contaTerminal.setNomeCliente(scanner.nextLine());
+
+        System.out.println("Por favor, valor do saldo!");
+        contaTerminal.setSaldo(scanner.nextDouble());
+        scanner.nextLine();
+
+        System.out.println("Olá " + contaTerminal.getNomeCliente() + ", obrigado por criar uma conta em nosso banco, sua agência é " + contaTerminal.getAgencia() + ", conta " + contaTerminal.getNumero() + " e seu saldo " + contaTerminal.getSaldo() + " já está disponível para saque");
+
+
     }
 }
